a2b5b64da10a4f7932f2c42810bc22db29519c85
Jiayi <j.xu@foundever.com>
2024-09-14 12:06:26 +0800

    update

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const urls = [
-    "https://justmysocks6.net/members/getbwcounter.php?service=1059848&id=596939ca-d377-4cb5-8ba5-d59ce98b4a60",
-    // Add more URLs here
-];
-
 async function fetchData() {
     const response = await fetch('bandwidth_data.json');
     return response.json();
--- update_data.py
+++ update_data.py
@@ -1,13 +1,10 @@
 import requests
 import json
-from datetime import datetime
+import os
 
-urls = [
-    "https://justmysocks6.net/members/getbwcounter.php?service=1059848&id=596939ca-d377-4cb5-8ba5-d59ce98b4a60",
-    # Add more URLs here
-]
+urls = os.environ['API_URLS'].split(',')
 
-data = [requests.get(url).json() for url in urls]
+data = [requests.get(url.strip()).json() for url in urls]
 
 with open('bandwidth_data.json', 'w') as f:
     json.dump(data, f)
